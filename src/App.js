ef43ae43393c35308c77b91af2fb4ed811bf8891
Ikenna <ikennaugwuh@gmail.com>
2017-05-23 23:46:01 -0400

    Added audio tag and search form.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import $ from 'jquery';
 import React, { Component } from 'react';
 import './App.css';
 
+$(document).on('submit', 'form', ()=>{ return false; });
+
 class App extends Component {
+  componentDidMount() {}
   render() {
     return (
-      <div className="App"></div>
+      <div className="App">
+        <div className="container">
+          <div className="audio-player">
+            <audio className="player"></audio>
+            <div className="info">Now Playing: <a className="loading" rel="noopener noreferrer" target="_blank">loading tracks...</a></div>
+          </div>
+          <div className="search-form">
+            <form action="#">
+              <input className="search-field" placeholder="search for music..."></input>
+            </form>
+          </div>
+        </div>
+      </div>
     );
   }
 }
