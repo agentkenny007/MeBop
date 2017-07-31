31370ce2fc6dcff08fed904562e4f1aec03c1a9f
Ikenna Asad <ikennaugwuh@gmail.com>
2017-07-31 15:22:51 -0400

    New component to add svg filters to app.

---
+++ src/components/misc.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import AudioPlayer from './AudioPlayer';
+
+export class Filters extends Component {
+  render() {
+    return (
+        <div id="filters">
+            <svg xmlns="http://www.w3.org/2000/svg"  version="1.1">
+                <defs>
+                    <filter id="blur">
+                        <feGaussianBlur stdDeviation="8"/>
+                    </filter>
+                </defs>
+            </svg>
+        </div>
+    );
+  }
+}
