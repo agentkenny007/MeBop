fb58ed4db93d6c688c8258cd6ef772fdc888be7b
Ikenna Asad <ikennaugwuh@gmail.com>
2017-08-01 00:30:29 -0400

    export default.

--- src/components/misc.js
+++ src/components/misc.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-export class Filters extends Component {
+export default class Filters extends Component {
   render() {
     return (
         <div id="filters">
-            <svg xmlns="http://www.w3.org/2000/svg"  version="1.1">
+            <svg>
                 <defs>
                     <filter id="blur">
                         <feGaussianBlur stdDeviation="8"/>
